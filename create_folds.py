0495c5d23e05a4ec182ad82da9e008786d47b302
Weston King-Leatham <westonkingl1@gmail.com>
2021-05-22 23:37:49 -0400

    adding main

--- create_folds.py
+++ create_folds.py
@@ -1,8 +1,17 @@
 import os
 import pandas as pd
+from sklearn import model_selection
 
 if __name__ == "__main__":
     # input_path = "E:\Users\Weston\workspace\Detecting-Melanoma\train.csv"
     input_path = "E:/Users/Weston/workspace/Detecting-Melanoma/"
     df = pd.read_csv(os.path.join(input_path, "train.csv"))
     df["kfold"] = -1
+    df = df.sample(frac=1).reset_index(drop=True) #shuffling dataframe
+    y = df.target.values
+    kf = model_selection.StratifiedKFold(n_splits=10)
+
+    for fold_, (, v_) in enumerate(kf.split(X=df, y=y)):
+        df.loc[v_, "kfold"] = fold_
+
+    print(df.target.value_counts())
---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+import torch
+
+import albumentations
+import pretrainedmodels
+
+import numpy as np
+import pandas as pd
+import torch.nn as nn
