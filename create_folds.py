dd48d9742d80010a55dfbc44a6e7592a89f9de86
Weston King-Leatham <westonkingl1@gmail.com>
2021-05-30 21:07:26 -0400

    Fixed fold number to 10

--- create_folds.py
+++ create_folds.py
@@ -10,12 +10,9 @@ if __name__ == "__main__":
     y = df.target.values
     kf = model_selection.StratifiedKFold(n_splits=10)
 
-    #for fold_, (, v_) in enumerate(kf.split(X=df, y=y)):
-    #    df.loc[v_, "kfold"] = fold_
-
-    for fold_, (_, _) in enumerate(kf.split(X=df, y=y)):
-        df.loc[:, "kfold"] = fold_
+    for fold_, (_, v_) in enumerate(kf.split(X=df, y=y)):
+        df.loc[v_, "kfold"] = fold_
 
     print(df.target.value_counts())
 
-    #df.to_csv(os.path.join(input_path, "train_folds.csv"), index=False)
+    df.to_csv(os.path.join(input_path, "train_folds.csv"), index=False)
